c48e30cfbeaf1ee5cd65385e87af098cd87a8b32
Segev Malool <segevmalool@Segevs-MacBook-Pro.local>
2022-03-03 16:22:55 -0800

    feature: use publisher combos and change to mono test result

--- project-reactor-test-runner/src/main/java/com/segbaus/projectreactor/TestExecutor.java
+++ project-reactor-test-runner/src/main/java/com/segbaus/projectreactor/TestExecutor.java
@@ -1,22 +1,61 @@
 package com.segbaus.projectreactor;
 
-import reactor.core.CorePublisher;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Signal;
+import reactor.core.scheduler.Schedulers;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class TestExecutor {
+import static java.util.Collections.synchronizedList;
 
-  public void execute(List<CorePublisher<Boolean>> testPublishers) {
+public class TestExecutor {
+  private List<Serializable> accumulatedTestResults;
+
+  public TestExecutor() {
+    this.accumulatedTestResults = synchronizedList(
+        new ArrayList<>()
+    );
+  }
+
+  /**
+   * The basic idea is that a test emits once with some test result data,
+   * then either completes or errors (in Reactor terminology, a Mono).
+   * Internally, there may be any number of conversions between
+   * CorePublishers, providing some level of flexibility.
+   *
+   * @param testPublishers A list of Reactor publishers representing test cases.
+   */
+  public void execute(
+      List<Mono<Serializable>> testPublishers
+  ) {
+
     if (testPublishers.isEmpty()) {
       return;
     }
 
+    System.out.println("main thread: " + Thread.currentThread());
+    System.out.println("found tests: " + testPublishers.toString());
+
     Flux.merge(testPublishers)
-        .doOnEach((Signal b) -> {
-          System.out.println(b);
+        .doOnEach((Signal s) -> {
+          // In any case
+          System.out.println("main thread: " + Thread.currentThread());
+          System.out.println(this.accumulatedTestResults.toString());
         })
-        .subscribe();
+        .doOnNext((Serializable testResult) -> {
+          this.submitResults(testResult.toString());
+        })
+        .onErrorContinue((Throwable err, Object o) -> {
+          this.submitResults(err.toString());
+        })
+        .subscribeOn(Schedulers.parallel())
+        .blockLast();
+  }
+
+  public void submitResults(Serializable result) {
+    accumulatedTestResults.add(result);
   }
 }
